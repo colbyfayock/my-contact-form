ae88666977186f8bd33b898e4ada0601072ca7f3
colbyfayock <fayock@gmail.com>
2023-01-03 16:39:37 -0500

    Step 7: Sending contact form data using SendGrid

--- functions/mail.tsx
+++ functions/mail.tsx
@@ -1,6 +1,24 @@
+const mail = require('@sendgrid/mail');
+
+mail.setApiKey(process.env.SENDGRID_API_KEY);
+
 exports.handler = async function (event, context) {
   const body = JSON.parse(event.body);
-  console.log('body', body);
+
+  const message = `
+    Name: ${body.name}rn
+    Email: ${body.email}rn
+    Message: ${body.message}
+  `;
+
+  await mail.send({
+    to: process.env.MAIL_TO,
+    from: process.env.MAIL_FROM,
+    subject: `New Message from ${body.name}!`,
+    text: message,
+    html: message.replace(/rn/g, '<br>'),
+  });
+
   return {
     statusCode: 200,
     body: JSON.stringify({ message: 'Success!' }),
