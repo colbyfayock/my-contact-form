e203e78486f74ad4279f84f454bdda0575b1c667
colbyfayock <fayock@gmail.com>
2023-01-03 15:12:37 -0500

    Step 5: Capturing contact form details

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,26 @@ import { FormRow } from './components/form-row/form-row';
 import { RiSendPlaneFill } from 'react-icons/ri';
 
 function App() {
+    function onFormSubmit(e: React.SyntheticEvent<HTMLFormElement>) {
+        e.preventDefault();
+
+        const formData: Record<string, string> = {};
+
+        function isInputNamedElement(e: Element): e is HTMLInputElement & { name: string } {
+            return 'value' in e && 'name' in e;
+        }
+
+        Array.from(e.currentTarget.elements).filter(isInputNamedElement).forEach((field) => {
+            if (!field.name) return;
+            formData[field.name] = field.value;
+        });
+    }
     return (
         <div className={styles.App}>
             <h1 className={styles.title}>Contact Us</h1>
             <p className={styles.description}>Have a question? Fill out the form below and we&apos;ll get back to you as soon as we can.</p>
             <p className={styles.note}>All fields are required</p>
-            <form className={styles.form}>
+            <form className={styles.form} onSubmit={onFormSubmit}>
                 <FormRow>
                     <Label htmlFor={'name'}>Name</Label>
                     <Input name={'name'} id={'name'} /></FormRow>
